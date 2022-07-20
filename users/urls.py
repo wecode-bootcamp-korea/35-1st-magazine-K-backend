237ac4a7686ab3ae006e88657d9f9e428fbc8263
xoxowo <1qq11212@gmail.com>
2022-07-21 00:25:30 +0900

    modified : 코드 컨벤션 수정

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from users.views import JoinView, LoginView
 
 urlpatterns = [
-    path('/join', JoinView.as_view() ),
+    path('/join', JoinView.as_view()),
     path('/login', LoginView.as_view()),
 ]

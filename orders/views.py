d7256a56444f4df74a55fd2ca5b03395233dd960
ehdrb92 <kcj920407@gmail.com>
2022-07-23 14:10:13 +0900

    Add : Cart Function

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views import View
+from django.http  import JsonResponse
+
+from core.utils.login_decorator import login_decorator
+from orders.models import Order
+
+class CartView(View):
+    @login_decorator
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            user         = request.user
+            order_status = data['order_status']
+
+            Order.objects.create(
+                user         = user,
+                order_status = order_status
+            )
+            
+            return JsonResponse({'message' : 'SUCCESE'}, status = 200)
+
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)

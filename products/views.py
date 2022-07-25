f6be77d5ceb06d6c76498a97d508bc13f82913d0
ehdrb92 <kcj920407@gmail.com>
2022-07-25 21:26:18 +0900

    Add : product list 2nd

--- products/views.py
+++ products/views.py
@@ -1,45 +1,41 @@
 from django.views     import View
 from django.http      import JsonResponse
+from django.db.models import Q
+from django.core.exceptions import FieldError
 
 from products.models import Product
 
 class ProductView(View):
     def get(self, request):
-        category = int(request.GET.get('category', 1))
-        offset   = int(request.GET.get('offset', 0))
-        limit    = int(request.GET.get('limit', 0))
-        sort_by  = request.GET.get('sort_by', 'newest')
+        try:
+            category = int(request.GET.get('category', 1))
+            offset   = int(request.GET.get('offset', 0))
+            limit    = int(request.GET.get('limit', 0))
+            sort_by  = request.GET.get('sort_by', '-issue_number')
 
-        products = Product.objects.filter(categoryproduct__category_id=category)
+            products = Product.objects.filter(Q(main_category=category) | Q(sub_category=category))
 
-        if sort_by == 'newest':
-            sorted_products = products.order_by('-issue_number')
+            sorted_products = products.order_by(sort_by)
 
-        if sort_by == 'oldest':
-            sorted_products = products.order_by('issue_number')
+            result = [
+                {
+                    'product_id'    : product.id,
+                    'title'         : product.title,
+                    'issue_number'  : product.issue_number,
+                    'main_category' : product.main_category.name,
+                    'price'         : product.price,
+                    'main_img_url_1': product.productimage.main_url,
+                    'main_img_url_2': product.productimage.sub_url,
+                }
+                for product in sorted_products[offset:offset+limit]]
 
-        if sort_by == 'high_to_low':
-            sorted_products = products.order_by('-price')
+            result.append(
+                {
+                    'category_total' : len(products)
+                }
+            )
 
-        if sort_by == 'low_to_high':
-            sorted_products = products.order_by('price')
+            return JsonResponse({'result' : result}, status = 200)
 
-        result = [
-            {
-                'product_id'    : product.id,
-                'title'         : product.title,
-                'issue_number'  : product.issue_number,
-                'main_category' : product.main_category.name,
-                'price'         : product.price,
-                'main_img_url_1': product.productimage.main_url,
-                'main_img_url_2': product.productimage.sub_url,
-            }
-            for product in sorted_products[offset:offset+limit]]
-
-        result.append(
-            {
-                'category_total' : len(products)
-            }
-        )
-
-        return JsonResponse({'result' : result}, status = 200)
+        except FieldError:
+            return JsonResponse({'message' : 'QUARY_PARAMETER_ERROR'}, status = 400)

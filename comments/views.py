8a6c148fd309136da72473f97282712c901f03ca
xoxowo <1qq11212@gmail.com>
2022-07-26 23:09:14 +0900

    comment-post 기능 구현

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views               import View
+from django.http                import JsonResponse
+
+from core.utils.login_decorator import login_decorator
+from comments.models            import Comment
+from products.models            import Product
+
+class CommentView(View):
+    @login_decorator
+    def post(self, request, product_id):
+        try :  
+            data    = json.loads(request.body)
+            user    = request.user
+            content = data['content'] 
+            rating  = data['rating']
+
+            Comment.objects.create(
+                user_id    = user.id,
+                content    = content,
+                rating     = rating,
+                product_id = product_id,
+            ) 
+            return JsonResponse({'MESSAGE':'SUCCESE'}, status=200)
+
+        except Product.DoesNotExist: 
+            return JsonResponse({'MESSAGE':'INVALID_PRODUCT'}, status=404)
+
+        except KeyError: 
+            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)

c39c6ce8812fcc716855be918964f2a8c8db5a9a
xoxowo <1qq11212@gmail.com>
2022-07-27 01:34:40 +0900

    comments-get 기능 구현

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views               import View
+from django.http                import JsonResponse
+
+from core.utils.login_decorator import login_decorator
+from comments.models            import Comment
+from products.models            import Product
+
+class CommentView(View):
+    def get(self, request, product_id):
+        try:
+            comments = Comment.objects.filter(product_id=product_id)
+    
+            results = [{
+                        'review'  : comment.id,
+                        'username': comment.user.username,
+                        'content' : comment.content,
+                        'rating'  : comment.rating,
+                         }for comment in comments]
+            return JsonResponse({'RESULTS':results}, status=200)      
+
+        except KeyError:
+            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400) 

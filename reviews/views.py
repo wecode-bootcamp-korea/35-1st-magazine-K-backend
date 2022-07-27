96e63c56b3cf594aa90e276516b653b1924301a7
xoxowo <1qq11212@gmail.com>
2022-07-27 16:45:37 +0900

    post 작성중 오류 발생..

--- reviews/views.py
+++ reviews/views.py
@@ -6,7 +6,8 @@ from django.http                import JsonResponse
 from core.utils.login_decorator import login_decorator
 
 from reviews.models             import Review
-from products.models            import Product
+from products.models            import Product  
+from orders.models              import Order, OrderStatus, OrderItem
 
 class ReviewView(View):
     @login_decorator
@@ -16,18 +17,20 @@ class ReviewView(View):
             user    = request.user
             content = data['content']
             rating  = data['rating']
-            product = Product.objects.get(id=product_id)
+
+            delivered = OrderStatus.objects.get(id=5)
+
+            orderd_products = OrderItem.objects.filter(order__user=user, order__order_status=delivered, product_id=product_id)
+            print(orderd_products)
+            if not OrderItem.objects.filter(order__user=user, order__order_status=delivered, product_id=product_id):
+                return JsonResponse({'MESSAGE':'INVALID_REQUEST'}, status=401)
 
             Review.objects.create(
-                user_id    = user.id,
+                user_id    = request.user.id,
                 content    = content,
                 rating     = rating,
-                product_id = product.id,
+                product_id = product_id,
             )
             return JsonResponse({'MESSAGE':'SUCCESE'}, status=200)
-
-        except Product.DoesNotExist:
-            return JsonResponse({'MESSAGE':'INVALID_PRODUCT'}, status=400)
-
         except KeyError:
             return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)

9a8e25c4cf05b4d0d5541eb8065dc06eb2ae8b55
xoxowo <1qq11212@gmail.com>
2022-07-27 16:57:57 +0900

    리뷰 반영 완료

--- reviews/views.py
+++ reviews/views.py
@@ -8,16 +8,12 @@ from products.models            import Product
 
 class ReviewView(View):
     def get(self, request, product_id):
-        try:
-            reviews = Review.objects.filter(product_id=product_id)
+        reviews = Review.objects.filter(product_id=product_id)
     
-            results = [{
-                        'review'  : review.id,
-                        'username': review.user.username,
-                        'content' : review.content,
-                        'rating'  : review.rating,
-                         }for review in reviews]
-            return JsonResponse({'RESULTS':results}, status=200)      
-
-        except KeyError:
-            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400) 
+        results = [{
+                    'review'  : review.id,
+                    'username': review.user.username,
+                    'content' : review.content,
+                    'rating'  : review.rating,
+                    }for review in reviews]
+        return JsonResponse({'RESULTS':results}, status=200)      

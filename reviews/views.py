b909fd6dbc70f4878bc51810041e8f1cc903f563
xoxowo <1qq11212@gmail.com>
2022-07-27 16:47:48 +0900

    review /get 수정

--- reviews/views.py
+++ reviews/views.py
@@ -3,21 +3,20 @@ import json
 from django.views               import View
 from django.http                import JsonResponse
 
-from core.utils.login_decorator import login_decorator
-from comments.models            import Comment
+from reviews.models             import Review
 from products.models            import Product
 
-class CommentView(View):
+class ReviewView(View):
     def get(self, request, product_id):
         try:
-            comments = Comment.objects.filter(product_id=product_id)
+            reviews = Review.objects.filter(product_id=product_id)
     
             results = [{
-                        'review'  : comment.id,
-                        'username': comment.user.username,
-                        'content' : comment.content,
-                        'rating'  : comment.rating,
-                         }for comment in comments]
+                        'review'  : review.id,
+                        'username': review.user.username,
+                        'content' : review.content,
+                        'rating'  : review.rating,
+                         }for review in reviews]
             return JsonResponse({'RESULTS':results}, status=200)      
 
         except KeyError:

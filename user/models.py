6a236092549c7117d893df5db439d71770671af9
ehdrb92 <kcj920407@gmail.com>
2022-11-18 14:46:00 +0900

    [fix] 유저 로그인시 아이디 대신 이메일 사용으로 기획 변경

--- user/models.py
+++ user/models.py
@@ -4,14 +4,13 @@ from core.models import BaseModel
 
 
 class User(BaseModel):
-    identification = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(max_length=100)
     password = models.CharField(max_length=200)
     name = models.CharField(max_length=20)
     zip_code = models.CharField(max_length=10, null=True)
     address_line_1 = models.CharField(max_length=100, null=True)
     address_line_2 = models.CharField(max_length=100, null=True)
     phone_number = models.CharField(max_length=50)
-    email = models.EmailField(max_length=100)
     point = models.DecimalField(max_digits=20, decimal_places=2, default=100000)
 
     class Meta:
